e3a3139da09856cd085fe6aedd07cbe85f7419e7
Kai Fink <kai.fink@yahoo.de>
2024-10-01 17:36:00 +0200

    fix: reformat entrypoint to array and also set new release to Chart file

--- prepare-release.sh
+++ prepare-release.sh
@@ -9,3 +9,12 @@ diff chart/values.yaml.old_tag chart/values.yaml.new_tag > chart/values.yaml.dif
 patch chart/values.yaml < chart/values.yaml.diff
 
 rm chart/values.yaml.*
+
+
+yq chart/Chart.yaml > chart/Chart.yaml.old_tag
+yq '.version = env(VERSION) | .appVersion = env(VERSION)' chart/Chart.yaml > chart/Chart.yaml.new_tag
+
+diff chart/Chart.yaml.old_tag chart/Chart.yaml.new_tag > chart/Chart.yaml.diff
+patch chart/Chart.yaml < chart/Chart.yaml.diff
+
+rm chart/Chart.yaml.*
